eff5ea5d4611e551d7fc95415ab839c7f8199868
eerciyes <enes.erciyes@amadeus.com>
2020-09-10 14:31:31 +0300

    UI Enhancements

---
+++ components/j-calendar.vue
@@ -0,0 +1,13 @@
+<template>
+  <j-calendar></j-calendar>
+</template>
+
+<script>
+import VCalendar from 'vuetify/lib/components/VCalendar/VCalendar'
+
+export default VCalendar.extend({
+  name: 'JCalendar'
+})
+</script>
+
+<style scoped></style>
--- pages/time-tracking/timesheet.vue
+++ pages/time-tracking/timesheet.vue
@@ -2,47 +2,85 @@
 <template>
   <v-layout>
     <v-flex>
-      <v-sheet height="600">
-        <v-calendar
-          ref="calendar"
-          :type="type"
-          :min-weeks="minWeeks"
-          :max-days="maxDays"
-          :now="now"
-          :dark="dark"
-          :weekdays="weekdays"
-          :first-interval="intervals.first"
-          :interval-minutes="intervals.minutes"
-          :interval-count="intervals.count"
-          :interval-height="intervals.height"
-          :interval-style="intervalStyle"
-          :show-interval-label="showIntervalLabel"
-          :short-intervals="shortIntervals"
-          :short-months="shortMonths"
-          :short-weekdays="shortWeekdays"
-          :color="color"
-          :events="events"
-          :event-overlap-mode="mode"
-          :event-overlap-threshold="45"
-          :event-color="getEventColor"
-          @change="getWorklogs"
-        ></v-calendar>
-      </v-sheet>
-      <!--<img src="/v.png" alt="Vuetify.js" class="mb-5" />
-    <blockquote class="blockquote">
-      &#8220;First, ask the problem. Then, write the code.&#8221;
-      <footer>
-        <small>
-          <em>&mdash;John Johnson</em>
-        </small>
-      </footer>
-    </blockquote> -->
+      <v-container>
+        <v-row no-gutters>
+          <v-col>
+            <v-row justify="start">
+              <v-avatar>
+                <img
+                  :src="$store.getters.getLoggedInUser.avatarUrl"
+                  alt="John"
+                />
+              </v-avatar>
+              <div class="wrapper-navbar">
+                <h2>{{ username }}</h2>
+              </div>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-toolbar flat color="white">
+        <v-btn outlined class="mr-4" color="grey darken-2" @click="setToday">
+          Today
+        </v-btn>
+        <v-btn fab text small color="grey darken-2" @click="prev">
+          <v-icon small>mdi-chevron-left</v-icon>
+        </v-btn>
+        <v-toolbar-title v-if="$refs.calendar">
+          {{ start }} - {{ end }}
+        </v-toolbar-title>
+        <v-btn fab text small color="grey darken-2" @click="next">
+          <v-icon small>mdi-chevron-right</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-container>
+        <v-sheet min-width="100%" height="80vh">
+          <v-calendar
+            ref="calendar"
+            v-model="focus"
+            :type="type"
+            :min-weeks="minWeeks"
+            :max-days="maxDays"
+            :now="now"
+            :dark="dark"
+            :weekdays="weekdays"
+            :first-interval="intervals.first"
+            :interval-minutes="intervals.minutes"
+            :interval-count="intervals.count"
+            :interval-height="intervals.height"
+            :interval-style="intervalStyle"
+            :show-interval-label="showIntervalLabel"
+            :short-intervals="shortIntervals"
+            :short-months="shortMonths"
+            :short-weekdays="shortWeekdays"
+            :color="color"
+            :events="events"
+            :event-overlap-mode="mode"
+            :event-overlap-threshold="45"
+            :event-color="getEventColor"
+            @change="getWorklogs"
+          ></v-calendar>
+        </v-sheet>
+      </v-container>
     </v-flex>
+    <!--<v-navigation-drawer
+      v-if="$store.getters.isAuthenticated"
+      v-model="drawer"
+      :clipped="true"
+      :permanent="true"
+      :mini-variant="true"
+      :right="true"
+      fixed
+      app
+    >
+    </v-navigation-drawer>-->
   </v-layout>
 </template>
 
 <script>
 import WorklogRetrievalService from '@/service/time-tracker/WorklogRetrievalService'
+// eslint-disable-next-line no-unused-vars
+import calendar from '@/layouts/calendar'
 
 const weekdaysDefault = [0, 1, 2, 3, 4, 5, 6]
 
@@ -90,31 +128,16 @@ const stylings = {
 export default {
   data: () => ({
     dark: false,
+    drawer: true,
+    focus: '',
+    start: '',
+    end: '',
     startMenu: false,
     endMenu: false,
     nowMenu: false,
     minWeeks: 1,
     now: null,
     events: [],
-    colors: [
-      'blue',
-      'indigo',
-      'deep-purple',
-      'cyan',
-      'green',
-      'orange',
-      'grey darken-1'
-    ],
-    names: [
-      'Meeting',
-      'Holiday',
-      'PTO',
-      'Travel',
-      'Event',
-      'Birthday',
-      'Conference',
-      'Party'
-    ],
     type: 'week',
     typeOptions: [
       { text: 'Day', value: 'day' },
@@ -185,7 +208,9 @@ export default {
     ],
     shortIntervals: true,
     shortMonths: false,
-    shortWeekdays: false
+    shortWeekdays: false,
+    startDate: '01-01-1900',
+    endDate: '01-08-1900'
   }),
   computed: {
     intervalStyle() {
@@ -196,14 +221,18 @@ export default {
     },
     hasEnd() {
       return this.type in { 'custom-weekly': 1, 'custom-daily': 1 }
+    },
+    username() {
+      return this.$store.getters.getLoggedInUser.username
     }
   },
   mounted() {
     this.$refs.calendar.scrollToTime('08:00')
+    this.$refs.calendar.checkChange()
   },
   methods: {
     viewDay({ date }) {
-      this.start = date
+      this.focus = date
       this.type = 'day'
     },
     getEventColor(event) {
@@ -213,6 +242,9 @@ export default {
       return interval.minute === 0
     },
     getWorklogs({ start, end }) {
+      this.start = start.date
+      this.end = end.date
+
       function generateEndDate(started, timeSpent) {
         const endDate = new Date(started)
         if (timeSpent.charAt(timeSpent.length - 1) === 'h') {
@@ -223,16 +255,16 @@ export default {
         }
         return endDate
       }
-      const events = []
-      console.log(start.date)
-      console.log(end.date)
+      const worklogs = []
+      // console.log(start.date)
+      // console.log(end.date)
       WorklogRetrievalService.retrieveWorklogs(start.date, end.date).then(
         (res) => {
           const JTTWorklogs = res.data
-          console.log(JTTWorklogs)
+          // console.log(JTTWorklogs)
           for (const JTTWorklog of JTTWorklogs.worklogs) {
-            console.log('IssueKey: ' + JTTWorklog.issueKey)
-            events.push({
+            // console.log('IssueKey: ' + JTTWorklog.issueKey)
+            worklogs.push({
               name: JTTWorklog.issueKey,
               start: new Date(JTTWorklog.started),
               end: generateEndDate(JTTWorklog.started, JTTWorklog.timeSpent),
@@ -243,8 +275,28 @@ export default {
         }
       )
 
-      this.events = events
+      this.events = worklogs
+    },
+    setToday() {
+      this.focus = ''
+    },
+    prev() {
+      console.log('Prev is called')
+      this.$refs.calendar.prev()
+    },
+    next() {
+      console.log('Next is called')
+      this.$refs.calendar.next()
     }
   }
 }
 </script>
+
+<style>
+.wrapper-navbar {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 10px;
+}
+</style>
