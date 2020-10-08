c3e0ee6c0e713ca3769a19b20bd25ea9ce1fba19
Mehmet Enes Erciyes <merciyes18@ku.edu.tr>
2020-10-08 11:37:33 +0300

    event update dialog added

--- components/calendar/j-event.vue
+++ components/calendar/j-event.vue
@@ -1,15 +1,19 @@
 <template>
-  <div class="vuecal__event-container">
-    <div class="vuecal__event-title">{{ worklog.title }}</div>
-    <div class="vuecal__event-explanation">
-      {{ worklog.explanation }}
-    </div>
-    <div class="vuecal__event-footer">
-      <v-icon small> mdi-check-outline </v-icon>
-      <div class="issueKey">{{ worklog.issueKey }}</div>
-      <strong class="timeSpent"> {{ worklog.timeSpent }}</strong>
-    </div>
-  </div>
+  <v-dialog>
+    <template v-slot:activator="{ on, attrs }">
+      <div class="vuecal__event-container" v-bind="attrs" v-on="on">
+        <div class="vuecal__event-title">{{ worklog.title }}</div>
+        <div class="vuecal__event-explanation">
+          {{ worklog.explanation }}
+        </div>
+        <div class="vuecal__event-footer">
+          <v-icon small> mdi-check-outline </v-icon>
+          <div class="issueKey">{{ worklog.issueKey }}</div>
+          <strong class="timeSpent"> {{ worklog.timeSpent }}</strong>
+        </div>
+      </div>
+    </template>
+  </v-dialog>
 </template>
 
 <script>
